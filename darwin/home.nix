ba5582979bf526e477fb25e9ed4a7fbe65124873
Steve Morgan <steve@rebelops.io>
2024-10-19 17:27:17 -0400

    fix: comment out configs

--- darwin/home.nix
+++ darwin/home.nix
@@ -17,10 +17,10 @@ in {
   home.stateVersion = "23.11";
 
   programs = {
-    tmux = import ../home/tmux.nix {inherit pkgs;};
-    zsh = import ../home/zsh.nix {inherit config pkgs lib; };
-    zoxide = (import ../home/zoxide.nix { inherit config pkgs; });
-    fzf = import ../home/fzf.nix {inherit pkgs;};
-    oh-my-posh = import ../home/oh-my-posh.nix {inherit pkgs;};
+    # tmux = import ../home/tmux.nix {inherit pkgs;};
+    # zsh = import ../home/zsh.nix {inherit config pkgs lib; };
+    # zoxide = (import ../home/zoxide.nix { inherit config pkgs; });
+    # fzf = import ../home/fzf.nix {inherit pkgs;};
+    # oh-my-posh = import ../home/oh-my-posh.nix {inherit pkgs;};
   };
 }
